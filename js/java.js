e56c6ab2747348378c891267b8d67bf455dae769
vlaad53 <163895253+vlaad53@users.noreply.github.com>
2025-02-10 16:40:07 +0200

    321

---
+++ js/java.js
@@ -0,0 +1,27 @@
+const celsius = 50;
+const farenheite = (celsius - 273.15) * 1.8 + 32;
+console.log(farenheite);
+
+
+const dayMonth = 30;
+const minutesMonth = dayMonth * 24 * 60 ;
+console.log(minutesMonth);
+
+
+const player = {
+    health:99, 
+    energy:80
+}
+player.health -=10;
+player.energy -=5;
+console.log(player);
+
+
+
+const totalPrice = 100;
+const discond = totalPrice * 0.10;
+const resaut = totalPrice - discond;
+console.log(resaut);
+
+
+
